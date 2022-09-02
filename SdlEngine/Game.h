dc1680b8c8e8021c600d8f1189a52905d361df20
DanielBrittainJr <djbrittain2017@gmail.com>
2022-09-01 23:37:44 -0400

    segmented the code, added and implemented the Game.cpp class and defintion. Currently able to create a fullscreen window.

--- SdlEngine/Game.h
+++ SdlEngine/Game.h
@@ -2,6 +2,7 @@
 
 #ifndef __Game__
 #define __Game__
+#include "SDL.h"
 
 class Game
 {
@@ -10,16 +11,17 @@ public:
 	~Game() {}
 
 	//set run var to true
-	void init() { m_bRunning = true; }
-
-	void render() {}
-	void update() {}
-	void handleEvents() {}
-	void clean() {}
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen); 
+	void render();
+//	void update();
+	void handleEvents();
+	void clean();
 
 	bool running() { return m_bRunning; }
 
 private:
+	SDL_Window* m_pWindow;
+	SDL_Renderer* m_pRenderer;
 
 	bool m_bRunning;
 
--- SdlEngine/main.cpp
+++ SdlEngine/main.cpp
@@ -5,54 +5,27 @@
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
 
-bool g_bRunning;
+//Game Object
+Game* g_game = 0;
 
 
 int main(int argc, char* argv[])
 {
-	if (init("Chapter 1: Setting up SDL", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN))
+	//create game instance 
+	g_game = new Game();
+
+	if (g_game->init("Chapter 1", 100, 100, 640, 480, true))
+
+	while (g_game->running())
 	{
-		g_bRunning = true;
-	}
-	else
-	{
-		return 1; //somethings wrong
+		g_game->handleEvents();
+		//g_game->update();
+		g_game->render();
 	}
 
-	//clean up sdl
-	SDL_Quit();
+	g_game->clean();
 
 	return 0;
 }
-bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
-{
-	//initialize SDL
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-	{
-		//if succeeded create our window
-		g_pWindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
 
-		// if the window creation succeeded create our renderer
-		if (g_pWindow != 0)
-		{
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
-	}
-	else
-	{
-		return false; //sdl could not initialize
-	}
 
-	return true;
-}
-
-void render()
-{
-	SDL_SetRenderDrawColor(g_pRenderer, 100, 25, 0, 200);
-
-	//clear the window to black
-	SDL_RenderClear(g_pRenderer);
-
-	//show the window
-	SDL_RenderPresent(g_pRenderer);
-}
