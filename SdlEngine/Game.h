b65a46421f8dba19e38dd9b05c80c3688dcd8969
DanielBrittainJr <djbrittain2017@gmail.com>
2022-09-01 22:35:43 -0400

    seperated main.cpp into an initializer and a renderer.

---
+++ SdlEngine/Game.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifndef __Game__
+#define __Game__
+
+class Game
+{
+public:
+	Game() {}
+	~Game() {}
+
+	//set run var to true
+	void init() { m_bRunning = true; }
+
+	void render() {}
+	void update() {}
+	void handleEvents() {}
+	void clean() {}
+
+	bool running() { return m_bRunning; }
+
+private:
+
+	bool m_bRunning;
+
+
+};
+
+#endif /* defined (__Game__)*/
